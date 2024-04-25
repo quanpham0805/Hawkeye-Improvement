1d3cf12b2b65330ed3776c9325bab48d397f0c97
quanpham0805 <shiroemon95@gmail.com>
2024-04-25 03:35:04 -0400

    local perceptron

--- src/perceptron.h
+++ src/perceptron.h
@@ -4,28 +4,36 @@
 #include <bitset>
 #include <cstdint>
 
-#define PERCEPTRON_TABLE_SIZE 1024  // Determined by threshold and history length
-#define THRESHOLD 127    // Floor(1.93 * HIST_LEN + 14) as specified in paper
-#define HIST_LEN 59      // Optimal value as determined by paper
+#define PERCEPTRON_TABLE_SIZE 2048  // Determined by threshold and history length
+#define THRESHOLD 44    // Floor(1.93 * HIST_LEN + 14) as specified in paper
+#define HIST_LEN 16      // Optimal value as determined by paper
 
 using namespace std;
 
 class PerceptronPredictor {
-  bitset<HIST_LEN> ghr;
-  uint32_t perceptron_step;
   int32_t perceptron_table[PERCEPTRON_TABLE_SIZE][HIST_LEN + 1];
+  bitset<HIST_LEN> lhr[PERCEPTRON_TABLE_SIZE];
 
   uint64_t hash_pc(uint64_t pc) {
-    uint64_t pcend = CRC(pc) % PERCEPTRON_TABLE_SIZE;
-    uint64_t ghrend = ((uint64_t)ghr.to_ullong()) % PERCEPTRON_TABLE_SIZE;
-    return pcend ^ ghrend;
+    return CRC(pc) % PERCEPTRON_TABLE_SIZE;
+  }
+
+  int get_weighted_sum(uint64_t hash) {
+    int sum = perceptron_table[hash][0];
+    for (size_t i = 1; i <= HIST_LEN; i++) {
+      if (lhr[hash][i - 1]) {
+        sum += perceptron_table[hash][i];
+      } else {
+        sum -= perceptron_table[hash][i];
+      }
+    }
+    return sum;
   }
 
  public:
   PerceptronPredictor() {
-    perceptron_step = 0;
-    ghr = bitset<HIST_LEN>();
     for (size_t i = 0; i < PERCEPTRON_TABLE_SIZE; i++) {
+      lhr[i] = bitset<HIST_LEN>(0);
       for (size_t j = 0; j < HIST_LEN + 1; j++) {
         perceptron_table[i][j] = 0;
       }
@@ -34,17 +42,7 @@ class PerceptronPredictor {
 
   bool get_prediction(uint64_t pc) {
     uint64_t hash = hash_pc(pc);
-    int sum = perceptron_table[hash][0];
-
-    for (size_t i = 1; i < HIST_LEN + 1; i++) {
-      if (ghr[i - 1]) {
-        sum += perceptron_table[hash][i];
-      } else {
-        sum -= perceptron_table[hash][i];
-      }
-    }
-    perceptron_step = abs(sum);
-    return sum >= 0;
+    return get_weighted_sum(hash) >= 0;
   }
 
   void update(uint64_t PC, bool result, bool prediction) {
@@ -53,15 +51,15 @@ class PerceptronPredictor {
     // Update the perceptron table entry only if the threshold has not been
     // reached or the predicted and true outcomes disagree. Update the bias
     // first, then the weights. Correct for overflow.
-    if (result != prediction || perceptron_step <= THRESHOLD) {
+    if (result != prediction || abs(get) <= THRESHOLD) {
       // If the branch was taken, increment the bias value. Else, decrement it.
 
       if (result) {
-        if (perceptron_table[hash][0] < THRESHOLD) {
-          perceptron_table[hash][0]++;
+        if (perceptron_table[hash][0] <= THRESHOLD) {
+          perceptron_table[hash][0]++; 
         }
       } else {
-        if (perceptron_table[hash][0] > -THRESHOLD) {
+        if (perceptron_table[hash][0] >= -THRESHOLD) {
           perceptron_table[hash][0]--;
         }
       }
@@ -70,7 +68,8 @@ class PerceptronPredictor {
 
       for (int i = 1; i < HIST_LEN + 1; i++) {
         // increase weight if prediction matches history
-        if (result == ghr[i - 1]) {
+
+        if (result == lhr[hash][i - 1]) {
           if (perceptron_table[hash][i] < THRESHOLD) {
             perceptron_table[hash][i]++;
           }
@@ -81,10 +80,9 @@ class PerceptronPredictor {
         }
       }
     }
-
-    // update the GHR by shifting left and setting new bit.
-    ghr = (ghr << 1);
-    ghr.set(0, result);
+    // update the LHR by shifting left and setting new bit.
+    lhr[hash] = (lhr[hash] << 1);
+    lhr[hash].set(0, result);
   }
 };
 
